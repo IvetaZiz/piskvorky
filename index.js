4c3a068fcba11f5859edbbc389f580cf31ba641c
Iveta Žižková <ziz.iveta@gmail.com>
2022-04-22 18:06:49 +0200

    Střídání hráčů

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 console.log('Funguju');
 
-const activePlayer = ['circle', 'cross'];
+let activePlayer = 'circle';
+const buttonChange = document.querySelectorAll('button');
+const playerChange = document.querySelector('.svg');
 
 const buttonClick = (event) => {
-  event.target.classList.add('.button__active__circle');
+  event.target.classList.add('button__active__circle');
+  if (activePlayer === 'circle') {
+    event.target.disabled = true;
+    activePlayer = 'cross';
+    playerChange.src = 'images/cross.svg';
+  } else {
+    event.target.classList.add('button__active__cross');
+    activePlayer = 'circle';
+    playerChange.src = 'images/circle.svg';
+  }
 };
-
-const buttonChange = document.querySelectorAll('button');
-for (let i = 0; i < buttonChange.length; i += 1) {
+for (let i = 0; i < buttonChange.length; i++)
   buttonChange[i].addEventListener('click', buttonClick);
-}
